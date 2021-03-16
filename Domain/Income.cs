837c8bb4303dc424e7863d6cbb85df56ff5e0963
garamvolgyiadam <49310198+garamvolgyiadam@users.noreply.github.com>
2021-03-16 20:35:16 +0100

    Created test database, (#1)
    Added DbContext

---
+++ Domain/Income.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public class Income
+    {
+        public double Amount { get; set; }
+
+        [MaxLength(255)]
+        public string Comment { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
