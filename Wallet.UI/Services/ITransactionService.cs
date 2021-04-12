ca3bbc55f0e76c33feb90440adf8a12007742b13
Demeter Görög <gorogdemi@outlook.com>
2021-04-12 11:07:47 +0200

    Added Transaction page skeletons (#6)
    * Added contracts.
    
    * Added Postgres support.
    
    * Changed namespaces.
    
    * Added Transaction page skeletons.

---
+++ Wallet.UI/Services/ITransactionService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wallet.Contracts.Requests;
+using Wallet.Contracts.Responses;
+
+namespace Wallet.UI.Services
+{
+    internal interface ITransactionService
+    {
+        Task<TransactionResponse> CreateAsync(TransactionRequest request);
+
+        Task<bool> DeleteAsync(int id);
+
+        Task<IEnumerable<TransactionResponse>> GetAsync();
+
+        Task<TransactionResponse> GetAsync(int id);
+
+        Task<bool> UpdateAsync(int id, TransactionRequest request);
+    }
+}
